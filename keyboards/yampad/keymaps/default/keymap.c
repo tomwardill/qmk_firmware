76f7962485a857d2b190733748f2c06637a0045e
Mattia Dal Ben <matthewdibi@gmail.com>
2019-05-18 12:01:36 +0200

    Added OLED screen support

--- keyboards/yampad/keymaps/default/keymap.c
+++ keyboards/yampad/keymaps/default/keymap.c
@@ -52,3 +52,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  _______,  RESET,     _______
   ),
 };
+
+#ifdef OLED_DRIVER_ENABLE
+void oled_task_user(void) {
+  // Host Keyboard Layer Status
+  oled_write_P(PSTR("Layer: "), false);
+  switch (biton32(layer_state)) {
+    case _BL:
+      oled_write_P(PSTR("Default\n"), false);
+      break;
+    case _FL:
+      oled_write_P(PSTR("Function\n"), false);
+      break;
+    default:
+      // Or use the write_ln shortcut over adding '\n' to the end of your string
+      oled_write_ln_P(PSTR("Undefined"), false);
+  }
+
+  // Host Keyboard LED Status
+  uint8_t led_usb_state = host_keyboard_leds();
+  oled_write_P(led_usb_state & (1<<USB_LED_NUM_LOCK) ? PSTR("NUMLCK ") : PSTR("       "), false);
+  oled_write_P(led_usb_state & (1<<USB_LED_CAPS_LOCK) ? PSTR("CAPLCK ") : PSTR("       "), false);
+  oled_write_P(led_usb_state & (1<<USB_LED_SCROLL_LOCK) ? PSTR("SCRLCK ") : PSTR("       "), false);
+}
+#endif
